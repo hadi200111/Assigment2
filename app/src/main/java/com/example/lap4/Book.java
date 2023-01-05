f839de41e2b5882a622e2481d57609616e4e4864
hadi200111 <1192016@student.birzeit.edu>
2023-01-05 06:55:36 +0200

    initial commit

---
+++ app/src/main/java/com/example/lap4/Book.java
@@ -0,0 +1,10 @@
+package com.example.lap4;
+
+public class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+    }
+}
---
+++ app/src/main/java/com/example/lap4/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.lap4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void firstButton(View view) {
+
+    }
+
+    public void secondButton(View view) {
+    }
+}
